f2e41c35ad39e9a4404649e8de96ef955e26697a
rikardny <rikardnystrom95@gmail.com>
2022-08-26 16:34:10 +0200

    prepare for changing state from array to object

--- src/index.js
+++ src/index.js
@@ -10,34 +10,34 @@ function Mode(props) {
 
   switch(props.mode) {
     case "setup":
-      change = <button type="button" className="btn change btn-primary" onClick={props.onClick}>Next</button>
+      change = <button type="button" className="btn change btn-primary" onClick={props.onChangeClick}>Next</button>
       mode = <button type="button" className="btn btn-primary" disabled>Setup</button>
       text = <span>1. Select initial plate positions</span>
       run = <button type="button" className="btn run btn-outline-primary" disabled>Run</button>
       break;
     case "source":
-      change = <button type="button" className="btn change btn-secondary" onClick={props.onClick}>Change</button>
+      change = <button type="button" className="btn change btn-secondary" onClick={props.onChangeClick}>Change</button>
       mode = <button type="button" className="btn btn-success" disabled>Source</button>
       text = <span>2. Select plate for pick up</span>
       run = <button type="button" className="btn run btn-outline-dark" disabled>Run</button>
       break;
     case "target":
-      change = <button type="button" className="btn change btn-secondary" onClick={props.onClick}>Cancel</button>
+      change = <button type="button" className="btn change btn-secondary" onClick={props.onChangeClick}>Cancel</button>
       mode = <button type="button" className="btn btn-danger" disabled>Target</button>
       text = <span>3. Select target position for plate</span>
       run = <button type="button" className="btn run btn-outline-secondary" disabled>Run</button>
       break;
     case "ready":
-      change = <button type="button" className="btn change btn-secondary" onClick={props.onClick}>Cancel</button>
+      change = <button type="button" className="btn change btn-secondary" onClick={props.onChangeClick}>Cancel</button>
       mode = <button type="button" className="btn btn-warning" disabled>Ready</button>
       text = <span>4. Press run to perform move</span>
-      run = <button type="button" className="btn run btn-warning">Run</button>
+      run = <button type="button" className="btn run btn-warning" onClick={props.onRunClick}>Run</button>
       break;
     case "moving":
       change = <button type="button" className="btn change btn-secondary" disabled>Cancel</button>
-      mode = <button type="button" className="btn btn-danger" disabled>Ready</button>
+      mode = <button type="button" className="btn btn-danger" disabled>Moving</button>
       text = <span>5. Press abort to stop move</span>
-      run = <button type="button" className="btn run btn-danger">Abort</button>
+      run = <button type="button" className="btn run btn-danger"onClick={props.onRunClick}>Abort</button>
       break;
     default: break;
   }
@@ -79,6 +79,7 @@ class Positions extends React.Component {
   render() {
     return (
       <div className="section map">
+        {this.renderSquare(0)}
         {this.renderSquare(1)}
         {this.renderSquare(2)}
         {this.renderSquare(3)}
@@ -93,11 +94,34 @@ class Positions extends React.Component {
 class System extends React.Component {
   constructor(props) {
     super(props);
+
+    const positions = ["f1", "f2", "f3", "f4", "m1"];
+    const obj = {};
+
+    for (const key of positions) {
+      obj[key] = "empty"
+    }
+
     this.state = {
       mode: "setup", // setup, source, target, ready, moving
       plates: Array(5).fill("empty"), // empty, full, source, target
       initial: Array(5).fill("empty"), // copy
+      plates2: obj,
+      initial2: structuredClone(obj)
     }
+  }
+
+  requestMove() {
+    console.log(this.state.plates)
+    fetch("http://localhost:5000/draw", {
+      mode: "no-cors"
+    })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => {
+      console.log("unable to fetch -", err);
+    });
   }
 
   resetPlates() {
@@ -128,19 +152,32 @@ class System extends React.Component {
     }
   }
 
-  handlePlateClick(i) {
+  handleRunClick() {
+    switch(this.state.mode) {
+      case "ready":
+        this.setState({mode: "moving"});
+        this.requestMove();
+        break;
+      case "moving":
+        this.setState({mode: "ready"});
+        break;
+      default: break;
+    }
+  }
+
+  handlePlateClick(id) {
     const plates = this.state.plates.slice();
     switch(this.state.mode) {
-      case "setup": plates[i] = plates[i] === "empty" ? "full" : "empty"; break;
+      case "setup": plates[id] = plates[id] === "empty" ? "full" : "empty"; break;
       case "source":
-        if (plates[i] === "full") {
-          plates[i] = "source";
+        if (plates[id] === "full") {
+          plates[id] = "source";
           this.setState({mode: "target"});
         }
         break;
       case "target":
-        if (plates[i] === "empty") {
-          plates[i] = "target";
+        if (plates[id] === "empty") {
+          plates[id] = "target";
           this.setState({mode: "ready"});
         }
         break;
@@ -154,7 +191,8 @@ class System extends React.Component {
       <div className="container">
         <Mode
           mode={this.state.mode}
-          onClick={() => this.handleChangeClick()}
+          onChangeClick={() => this.handleChangeClick()}
+          onRunClick={() => this.handleRunClick()}
         />
         <Positions
           plates={this.state.plates}
