5f2d5feec8b79cb0a1f79d045c1e5d9008c6ff3d
rikardny <rikardnystrom95@gmail.com>
2022-10-14 17:11:57 +0200

    changed install file

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Using Ubuntu
+# Install nodejs
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-cd ~/Desktop/monster
 
+
+# Install Dorna API
 git clone https://github.com/dorna-robotics/dorna2-python.git
 cd dorna2-python
 sudo python3 setup.py install --force
 
-cd ../server
-pip install flask networkx
+# Install
